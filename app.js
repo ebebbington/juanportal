d98bf8e5462077066431e438a5c56e79b25c98ce
Edward Bebbington <edward.bebbington@intericty.technology>
2019-04-12 09:54:33 +0100

    added ecosystem, completed error.pug

--- app.js
+++ app.js
@@ -6,14 +6,11 @@ const people = JSON.parse(fs.readFileSync('./people.json'))
 const server = app.listen(port, () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
-
 // Set the viewing engine
 app.set('view engine', 'pug')
-
-// Server static files from the public folder
+// Serve static files from the public folder
 app.use(express.static(__dirname + '/public'))
-
-// On / render index.pug in views/ as pug expects it to be in views
+// On '/' render index.pug in views/ as pug expects it to be in views
 app.get('/', (req, res) => {
   res.render('index', { // pass in variables to the file
     name: 'Edward',
@@ -21,23 +18,26 @@ app.get('/', (req, res) => {
     people: people.profiles
   });
 });
-
+// On '/profiles' render profiles page getting the ID sent across
 app.get('/profile', (req, res) => {
-  let id = req.query.id
-  id = Number(id)
+  let id = Number(req.query.id)
   let person = []
   for (let i = 0; i < people.profiles.length; i++) {
     if (id === people.profiles[i].id) {
       person.push(people.profiles[ i ])
     }
   }
-  res.render('profile', {
-    title: `About ${person.name}`,
-    person
-  })
+  if (person.length > 0) {
+    res.render('profile', {
+      title: `About ${person.name}`,
+      person
+    })
+  } else {
+    res.send('No ID has been found')
+  }
 })
 
-server.on('error', function (error) {
+server.on('error', function (error) {ff
   res.render('error', {
     errorMsg: error
   })
---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports =
+  {
+    apps:
+      [
+        {
+          name: 'JuanPortal',
+          script: '/mnt/c/xampp/htdocs/juanportal/app.js'
+        }
+      ]
+  };
