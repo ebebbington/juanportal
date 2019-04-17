bd910554b3c22f0538dfd2ac867d87cff04f84d8
Edward Bebbington <edward.bebbington@intericty.technology>
2019-04-17 16:37:18 +0100

    Replaced profile.js database code with mongoose code

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,47 @@ const app = express()
 const fs = require('fs')
 const people = JSON.parse(fs.readFileSync('/mnt/c/xampp/htdocs/juanportal/people.json'))
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 app.use(morgan('tiny'))
+
+
+// ///////////////////////////////
+// Get Profiles Promise
+// ///////////////////////////////
+/*
+const profiles = new Promise(function (resolve, reject) {
+  mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true })
+  const profileSchema = new mongoose.Schema({
+    'name': String,
+    'description': String
+  })
+  const Profile = mongoose.model('Profile', profileSchema)
+  Profile.find(function (err, res) {
+    if (err) {
+      console.error(err)
+    }
+    // console.log('FIRST RESULT', res)
+    // console.log('FIRST RESULT LENGTH', res.length)
+    // console.log('FIRST RESULT TYPEOF', typeof res)
+
+    for(let i=0,l=res.length; i<l;i++){
+      console.log('IN LOOP WHOLE OBJ', res[i])
+      console.log('IN LOOP ID', res[i]._id)
+      console.log('IN LOOP NAME', res[i]["name"])
+    }
+
+    resolve(res)
+  })
+})
+
+profiles
+  .then(function (resolved) {
+    console.log(typeof resolved[0])
+    console.log('Data of user 0:', resolved[0])
+    console.log('Data of user 0s name:', resolved[0].name)
+  })
+*/
 
 // On '/' render index.pug in views/ as pug expects it to be in views
 app.get('/', (req, res) => {
--- routes/profile.js
+++ routes/profile.js
@@ -1,18 +1,39 @@
 //
 // URL: localhost:3005/profile
 //
-
 const express = require('express')
 const app = express()
 const fs = require('fs')
 const people = JSON.parse(fs.readFileSync('/mnt/c/xampp/htdocs/juanportal/people.json'))
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 app.use(morgan('tiny'))
+
+//
+// Prepare profile model
+//
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true })
+const profileSchema = new mongoose.Schema({
+  'name': String,
+  'description': String
+})
+const Prof = mongoose.model('Profile', profileSchema)
 
 // On '/profiles' render profiles page getting the ID sent across
 app.get('/', (req, res) => {
   let id = Number(req.query.id)
+  let person = []
+  Prof.find({ id: id }, function (err, profile) {
+    console.log(profile)
+    res.render('profile', {
+      title: `About ${profile.name}`,
+      name: profile[0].name,
+      description: profile[0].description
+    })
+  })
+
+  /* Old way
   let person = []
   for (let i = 0; i < people.profiles.length; i++) {
     if (id === people.profiles[i].id) {
@@ -26,7 +47,7 @@ app.get('/', (req, res) => {
     })
   } else {
     res.send('No ID has been found')
-  }
+  } */
 })
 
 module.exports = app
