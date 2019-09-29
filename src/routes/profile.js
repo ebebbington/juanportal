9c33f853cd4cad52994a1a16354197ca506a1c2c
ebebbington <EdwardSBebbington@hotmail.com>
2019-09-29 19:16:59 +0100

    all profiles routes are now correctly handled here

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -14,10 +14,49 @@ const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
 const { check, validationResult, body } = require('express-validator/check')
 const { sanitiseBody } = require('express-validator/filter')
+const addView = 'add'
 
 app.use(morgan('tiny'))
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
+
+function generateImageName (imgExt) {
+  const imgPath = './images/'
+    + Math.random().toString(36).substring(2, 15)
+    + Math.random().toString(36).substring(2, 15)
+    + '.' + imgExt
+    return imgPath
+}
+
+function saveProfile (name, description, image, imgPath, res) {
+  // create the document
+  mongoose.connect(dbUrl, { useNewUrlParser: true })
+  const newProfile = new Profile({
+    name: name,
+    description: description,
+    image: imgPath
+  })
+  // validation
+  newProfile.validate(function (err) {
+    if (err) {
+      return res.status(500).send({error: 'Problem validating your new profile'})
+    }
+  })
+  // save image if one
+  if (image) {
+    fs.writeFile(imgPath, image.buffer, 'base64', function (err) {
+      if (err) {
+        return res.status(500).send({error: 'Problem saving the supplied image'})
+      }
+    })
+  }
+  // save to mongoose
+  newProfile.save(function (err) {
+    if (err) {
+      return res.status(500).send({error: 'Problem saving this to the database'})
+    }
+  })
+}
 
 // On '/profiles' render profiles page getting the ID sent across
 //
@@ -28,10 +67,12 @@ app.get('/', (req, res) => {
   mongoose.connect(dbUrl, { useNewUrlParser: true})
   Profile.findOne({ _id: id }, function (err, profile) {
     // If error
-    if (err) res.end('Error:', err)
+    if (err) {
+      return res.status(500).send({error: 'Problem fetching profiles'})
+    }
     // Check if data was pulled
     if (!profile) {
-      res.end('No id was found')
+      return res.status(404).send({error: 'No profiles were found'})
     } else {
       res.render('profile', {
         title: `About ${profile.name}`,
@@ -42,31 +83,16 @@ app.get('/', (req, res) => {
     }
     mongoose.disconnect()
   })
-
-  /* Old way
-  let person = []
-  for (let i = 0; i < people.profiles.length; i++) {
-    if (id === people.profiles[i].id) {
-      person.push(people.profiles[ i ])
-    }
-  }
-  if (person.length > 0) {
-    res.render('profile', {
-      title: `About ${person[0].name}`,
-      person
-    })
-  } else {
-    res.send('No ID has been found')
-  } */
 })
 
+// On selecting to create a new profile
 app.get('/add', (req, res) => {
-  const title = 'Add a Profile'
   res.render('add', {
-    title: title
+    title: 'Add a profile'
   })
 })
 
+// On submititng the new profile
 app.post('/add', upload.single('image'), [
   // Sanitisation
   body('name')
@@ -78,69 +104,6 @@ app.post('/add', upload.single('image'), [
     .trim()
     .escape(),
 ], function (req, res) {
-  console.log('im here')
-  /*
-  * Can access props of image.
-  * Can also access buffer stream with imageFile.buffer or use a foreach on it like:
-  * imageFil.buffer.forEach(function (val) { console.log(val) }
-  * This way is only acieved by having the emctype attribute in pug. The object
-  * holds all file data
-   */
-  // const imageFile = req.file
-  // fs.writeFileSync('./images/base64.png', imageFile.buffer, 'base64', function (err) {
-  //   if (err) return console.log(err)
-  // })
-  /*
-  * The below code were my multiple methods of downloading and saving a file
-  *
-  //
-  // Method 1 - Check data packets being sent
-  //
-  let imageData = ''
-  req.on('data', function (chunk) {
-    console.log(Buffer.from(req.file.toString()))
-    console.log('on data')
-    imageData += chunk
-  })
-  req.on('end', function () {
-    fs.writeFile('./images/test2.png', imageData, 'binary', function (err) {
-      console.log('on end')
-      if (err) throw err
-      console.log('file saved')
-    })
-  })
-  return
-
-  //
-  // Method 3
-  //
-  // write file using binary
-  const test = Buffer.from(req.body.image, 'binary')
-  fs.writeFile('./images/test.png', test, 'binary', function (err) {
-    if (err) return console.log(err)
-  })
-  console.log(test)
-  console.log('--------------------')
-  //
-  // Method 4
-  //
-  // testing in base64 and using writeFileSync instead of writeFile
-  const test1 = Buffer.from(req.body.image, 'base64')
-  fs.writeFileSync('./images/test1.png', test1, function (err) {
-    if (err) return console.log(err)
-  })
-  console.log(test)
-  console.log('--------------------')
-
-  //
-  // Method 5
-  //
-  const image = req.file
-  console.log(image)
-  console.log('-----------------------')
-
-  return
-  */
   // Check for errors in sanitisation
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
@@ -150,63 +113,45 @@ app.post('/add', upload.single('image'), [
   const name = String(req.body.name)
   const description = String(req.body.description)
   const image = req.file
-  let imageName = ''
-  image ? imageName = image.originalname : imageName = 'sample.jpg'
-  const imagePath = './images/' + imageName
-  // check image extension
-  const acceptedImageExtensions = ['.png', '.jpg']
-  const imageExtension = imageName.substr(imageName.length - 4).toLowerCase()
-  if (acceptedImageExtensions.indexOf(imageExtension) === -1) {
-    res.render('add-profile', {
-      title: title,
+  console.log(image)
+  if (!image) {
+    const imageName = './images/sample.jpg'
+    saveProfile(name, description, null, imageName, res)
+    return res.redirect('/')
+  } 
+  // check the extension of the image name
+  const imgExt = image.originalname.split('.').pop()
+  const acceptedImageExtensions = ['png', 'jpg', 'jpeg']
+  if (acceptedImageExtensions.indexOf(imgExt) < 0) {
+    return res.render('add', {
+      title: 'Add title',
       imageErr: 'Wrong format'
     })
-    return
   }
-  // check data is set
-  if (!name) {
-    res.render('add-profile', {
-      title: 'Add a Profile', nameErr: 'Name is not set'
+  // generate the unique name for the image
+  const imgPath = generateImageName(imgExt)
+  // save he new profile
+  saveProfile(name, description, image, imgPath, res)
+  return res.redirect('/')
+})
+
+app.get('/delete', (req, res) => {
+  const ObjectId = mongoose.Types.ObjectId
+  try {
+    const id = new ObjectId(req.query.id)
+    mongoose.connect(dbUrl, { useNewUrlParser: true })
+    Profile.deleteOne({ _id: id }, function (err) {
+      if (err) {
+        mongoose.disconnect()
+        return res.status(500).send({ errors: 'Problem deleting that profile' })
+      } else {
+        mongoose.disconnect()
+      return res.redirect('/')
+      }
     })
-    return
+  } catch (e) {
+    return res.status(400).send({error: 'You trying to delete a random profile?'})
   }
-  if (!description) {
-    res.render('add-profile',
-      {title: 'Add a Profile', descriptionErr: 'Description is not set'
-      })
-    return
-  }
-  // Create the path to the file
-  let absoluteImagePath = ''
-  if (image) { // path to enw file
-    absoluteImagePath = '/images/' + imageName
-  } else { // path to sampke image is not supplied
-    absoluteImagePath = '/images/sample.jpg'
-  }
-  // create the document
-  mongoose.connect(dbUrl, { useNewUrlParser: true })
-  const newProfile = new Profile({
-    name: name,
-    description: description,
-    image: absoluteImagePath
-  })
-  // validation
-  newProfile.validate(function (err) {
-    if (err) return console.log(err)
-  })
-  // save image if one is supplied
-  if (image) {
-    fs.writeFile(imagePath, image.buffer, 'base64', function (err) {
-      if (err) return console.log(err)
-    })
-  }
-  // save to mongoose
-  newProfile.save(function (err) {
-    if (err) return console.log(err)
-  })
-  //mongoose.disconnect()
-  // redirect
-  res.redirect('/')
 })
 
 module.exports = app
