b13d0e707a3809a502a882a2bd277a9f3dda000f
Edward Bebbington <edward.bebbington@intercity.technology>
2019-11-26 11:31:46 +0000

    added old logger file

---
+++ src/data/logger.js
@@ -0,0 +1,14 @@
+module.exports = function (request, response, next) {
+  const start = +new Date()
+  const method = request.method
+  const url = request.url
+  const ip = request.connection.remoteAddress
+  const stream = process.stdout
+  
+  response.on('finish', function () {
+    const duration = (+new Date() - start)
+    const message = method + ' to ' + url + ' took ' + duration + 'ms from ' + ip + '\n\n'
+    stream.write(message)
+  })
+  next()
+}
