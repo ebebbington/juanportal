eb3df680bb8c209fe3b5541e282837c905cc0f16
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-11-27 23:12:35 +0000

    added a base model

---
+++ src/models/BaseModel.ts
@@ -0,0 +1,37 @@
+class BaseModel {
+
+    /**
+   * Validate object properties against the schema before submitting to the database
+   * 
+   * @param {object} model The object model to validate
+   * 
+   * @return {object|undefined} Set to undefined if no errors
+   */
+  protected validateInputFields (model: any): any {
+    return model.validateSync()
+  }
+
+  /**
+   * Validate output fields
+   * 
+   * Before returning a model extracted from the database,
+   * strip out any properties that arent defined in the fieldsToExpose
+   * array, as suggested. It looks through the object properties and then
+   * checks every field to expose against that. Rinse and repeat
+   * 
+   * @param {object} model The model object retrieved from the database 
+   * 
+   * @return {object} The same passed in model but stripping the non-exposable fields
+   */
+  protected validateOutputFields (model: any, fieldsToExpose: string[]): any {
+    Object.keys(model).forEach((profileProp) => {
+      fieldsToExpose.forEach((field) => {
+        if (profileProp !== field) // remove the key from the object as we dont want toe xpose it
+          delete model.item
+      })
+    })
+    return model
+  }
+}
+
+module.exports = BaseModel
