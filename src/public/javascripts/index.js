104678f400b74b581c21449918dfa9e6000bc7f5
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-11-30 20:20:09 +0000

    now handling the delete a profile action

--- src/public/javascripts/index.js
+++ src/public/javascripts/index.js
@@ -2,6 +2,17 @@
 
 $(document).ready(function () {
   $('.delete-profile').on('click', function () {
-
+    const id = $(this).data('id')
+    const profile = $(this).closest('.person')
+    $.ajax({
+      method: 'delete',
+      url: '/profile/id/' + id,
+      success: function () {
+        profile.remove()
+      },
+      error: function (err) {
+        profile.remove()
+      }
+    })
   })
 })
