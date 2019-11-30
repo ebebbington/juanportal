618e66b7950ec0251edcf3d8f423fff407319dd8
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-11-30 20:20:55 +0000

    imrpoved routing for profiles

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const app = express()
 const ProfileController = require('../controllers/ProfileController.js')
 
 // For when an image is submited in the form when POSTing a profile
@@ -7,22 +8,13 @@ const multer = require('multer')
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
 
-router
-  /**
-  * GET /profile?id
-  */
-  .get("/", ProfileController.get)
-  /**
-   * GET /profile/add
-   */
-  .get('/add', ((req, res) => { res.render('profile/add', {title: 'Add a profile'})}))
-  /**
-   * POST /profile/add
-   */
-  .post('/add', upload.single('image'), ProfileController.post)
-  /**
-   * GET /profile/delete?id
-   */
-  .get('/delete', ProfileController.delete)
+app.route('/id/:id')
+  .get(ProfileController.get)
+  .delete(ProfileController.delete)
+  .put(ProfileController.update)
 
-module.exports = router
+app.route('/add')
+  .get((req, res) => { res.render('profile/add', {title: 'Add a profile'})})
+  .post(upload.single('image'), ProfileController.post)
+
+module.exports = app
