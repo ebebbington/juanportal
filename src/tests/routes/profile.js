bded099b03958c800ff4a6d2840be9c1a5cd8885
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-12-01 20:14:04 +0000

    completed tests for profile routing

--- src/tests/routes/profile.js
+++ src/tests/routes/profile.js
@@ -4,6 +4,7 @@ const expect = chai.expect
 const app = require('../../app')
 const chaiHttp = require('chai-http')
 const ProfileModel = require('../../models/ProfileModel')
+const Profile = new ProfileModel()
 const fs = require('fs')
 const logger = require('../../helpers/logger')
 const mongoose = require('mongoose')
@@ -29,7 +30,7 @@ describe('Profile Route', () => {
    *
    * Test adding a user, which we will also use further down
    */
-  describe('POST /profile/add', () => {
+  describe.only('POST /profile/add', () => {
     const newTestProfile = {
       name: 'Super wierd odd test account name',
       description: 'Hello world',
@@ -46,14 +47,61 @@ describe('Profile Route', () => {
         .field('description', newTestProfile.description)
         .attach('image', fs.readFileSync('/var/www/juanportal/public/images/sample.jpg'), 'sample.jpg')
         .end((err, res) => {
-          expect(res.status, 200)
+          expect(res.status).to.equal(200)
           done();
         });
     })
-    it('Should fail when name doesnt meet validation')
-    it('Should fail when image doesnt meet validation')
+    it('Should fail when name doesnt meet validation', (done) => {
+      chai.request(app)
+        .post('/profile/add', upload.single('image'))
+        .field('name', 'a')
+        .field('description', newTestProfile.description)
+        .attach('image', fs.readFileSync('/var/www/juanportal/public/images/sample.jpg'), 'sample.jpg')
+        .end((err, res) => {
+          expect(res.status).to.equal(400)
+          const json = JSON.parse(res.text)
+          expect(json.success).to.equal(false)
+          expect(json.message).to.have.haveOwnProperty('name')
+          done();
+        });
+    })
+    it('Should pass when no description is passed in', (done) => {
+      chai.request(app)
+      .post('/profile/add', upload.single('image'))
+      .field('name', newTestProfile.name)
+      .field('description', '')
+      .attach('image', fs.readFileSync('/var/www/juanportal/public/images/sample.jpg'), 'sample.jpg')
+      .end((err, res) => {
+        expect(res.status).to.equal(200)
+        done();
+      });
+    })
+    it('Should fail when image doesnt meet validation', (done) => {
+      chai.request(app)
+        .post('/profile/add', upload.single('image'))
+        .field('name', newTestProfile.name)
+        .field('description', newTestProfile.description)
+        .attach('image', fs.readFileSync('/var/www/juanportal/public/images/test.txt'), 'test.txt')
+        .end((err, res) => {
+          expect(res.status).to.equal(400)
+          const json = JSON.parse(res.text)
+          expect(json.success).to.equal(false)
+          expect(json.message).to.have.haveOwnProperty('image')
+          done();
+        });
+    })
+    it('Should pass when no image is passed in', (done) => {
+      chai.request(app)
+        .post('/profile/add', upload.single('image'))
+        .field('name', newTestProfile.name)
+        .field('description', newTestProfile.description)
+        .end((err, res) => {
+          expect(res.status).to.equal(200)
+          done();
+        });
+    })
     it('Should have saved the user', (done) => {
-      ProfileModel.findOneByName(newTestProfile.name)
+      Profile.getOneByName(newTestProfile.name)
         .then((profile) => {
           expect(profile.name).to.equal(newTestProfile.name)
           expect(profile.description).to.equal(newTestProfile.description)
@@ -61,14 +109,41 @@ describe('Profile Route', () => {
         })
     })
     it('Should have created a file', (done) => {
-      ProfileModel.findOneByName(newTestProfile.name)
+      Profile.getOneByName(newTestProfile.name)
       .then((profile) => {
         const filePath = '/var/www/juanportal' + profile.image
         expect(chaiFile(filePath)).to.exist
         done()
       })
     })
-    // after('Remove the test user')
+    // it('Should not be allowed to create duplicate profiles', () => {
+    //   chai.request(app)
+    //     .post('/profile/add', upload.single('image'))
+    //     .field('name', newTestProfile.name)
+    //     .field('description', newTestProfile.description)
+    //     .end((err, res) => {
+    //       chai.request(app)
+    //         .post('/profile/add', upload.single('image'))
+    //         .field('name', newTestProfile.name)
+    //         .field('description', newTestProfile.description)
+    //         .end((err, res) => {
+    //           expect(res.status).to.equal(400)
+    //           done();
+    //         });
+    //     });
+    // })
+    // afterEach('Remove the test user', (done) => {
+    //   Profile.getOneByName(newTestProfile.name)
+    //     .then((profile) => {
+    //       const id = profile._id
+    //       chai.request(app)
+    //         .delete('/profile/id/' + id)
+    //         .end((err, res) => {
+    //           expect(res.status, 200)
+    //           done();
+    //         });
+    //     })
+    // })
   })
 
   describe('GET /profile/id/:id', () => {
