561f4138129d9bfc9eda16bb4e1b41abc6703609
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-12-01 02:22:43 +0000

    developed profile route test alot

--- src/tests/routes/profile.js
+++ src/tests/routes/profile.js
@@ -8,99 +8,107 @@ const fs = require('fs')
 const logger = require('../../helpers/logger')
 const mongoose = require('mongoose')
 const ProfileController = require('../../controllers/ProfileController')
+const chaiFiles = require('chai-files')
 
 const multer = require('multer')
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
 
+const chaiFile = chaiFiles.file
 chai.use(chaiAsPromised)
 chai.use(chaiHttp)
 chai.should()
 
-describe('Profile Route', () => {
-  const newTestProfile = {
-    name: 'Super wierd odd test account name',
-    description: 'Hello world',
-    image: function () {
-      return fs.readFile('../../public/images/sample.jpg', (data) => {
-        return data
-      })
-    }
-  }
+// turn off logging
+logger.info = function (a) {}
+logger.debug = function (e) {}
 
+describe('Profile Route', () => {
   /**
    * POST /profile/add
    *
    * Test adding a user, which we will also use further down
    */
-  it('Should POST /profile/add', (done) => {
-    chai.request(app)
-      .post('/profile/add', upload.single('image'))
-      .field('name', newTestProfile.name)
-      .field('description', newTestProfile.description)
-      .attach('image', fs.readFileSync('/var/www/juanportal/public/images/sample.jpg'), 'sample.jpg')
-      .end((err, res) => {
-        expect(res.status, 200)
-        done();
-      });
-  })
-
-  let newUserId = null
-  let newUserImage = null
-  before('Get the test account id', () => {
-    ProfileModel.getOneByName(newTestProfile.name)
+  describe('POST /profile/add', () => {
+    const newTestProfile = {
+      name: 'Super wierd odd test account name',
+      description: 'Hello world',
+      image: function () {
+        return fs.readFile('../../public/images/sample.jpg', (data) => {
+          return data
+        })
+      }
+    }
+    it('Should respond with a 200 with valid data', (done) => {
+      chai.request(app)
+        .post('/profile/add', upload.single('image'))
+        .field('name', newTestProfile.name)
+        .field('description', newTestProfile.description)
+        .attach('image', fs.readFileSync('/var/www/juanportal/public/images/sample.jpg'), 'sample.jpg')
+        .end((err, res) => {
+          expect(res.status, 200)
+          done();
+        });
+    })
+    it('Should fail when name doesnt meet validation')
+    it('Should fail when image doesnt meet validation')
+    it('Should have saved the user', (done) => {
+      ProfileModel.findOneByName(newTestProfile.name)
+        .then((profile) => {
+          expect(profile.name).to.equal(newTestProfile.name)
+          expect(profile.description).to.equal(newTestProfile.description)
+          done()
+        })
+    })
+    it('Should have created a file', (done) => {
+      ProfileModel.findOneByName(newTestProfile.name)
       .then((profile) => {
-        logger.info(['IDS', profile._id, new mongoose.Types.ObjectId(profile._id)])
-        newUserImage = profile.image
-        newUserId = profile._id
+        const filePath = '/var/www/juanportal' + profile.image
+        expect(chaiFile(filePath)).to.exist
+        done()
       })
+    })
+    // after('Remove the test user')
   })
 
-  /**
-   * GET /profile?id=
-   *
-   * Test the route
-   */
-  it('Should GET /profile?id=[...]', () => {
-    ProfileModel.findOneById(newUserId)
-      .then((profile) => {
-        logger.info(profile._id)
-        logger.info(newUserId)
-        expect(profile._id).to.equal(newUserId)
-      })
-      .catch((err) => {
-        logger.error(err)
-      })
+  describe('GET /profile/id/:id', () => {
+    it('Should get a profile with a valid id', () => {
+      ProfileModel.getOneByName(newTestProfile.name)
+        .then((profile) => {
+          const id = profile._id
+          chai.request(app)
+            .get('/profile/id/' + id)
+            .end((err, res) => {
+              expect(res.status, 200)
+              done();
+            });
+        })
+    })
+    it('Should fail when getting a profile with an invalid id')
   })
 
-  /**
-   * GET /profile/add
-   *
-   * Get the webpage to add a user
-   */
-  it('Should GET /profile/add', (done) => {
-    chai.request(app)
+  describe('GET /profile/add', () => {
+    it('Should respond with a 200', (done) => {
+      chai.request(app)
       .get('/profile/add')
       .end((err, res) => {
-        expect(res.status, 200)
+        expect(res.status).to.equal(200)
         done();
       });
+    })
   })
 
-  /**
-   * GET /profile/delete?id=
-   *
-   * Delete a profile
-   */
-  it('Should GET /profile/delete?id=', () => {
-    // add a test user then delet eby the object id
-  }) // GET /profile/delete?id=jjkkj
-
+  describe('DELETE /profile/id/:id', () => {
+    it('Should respond with 200 on valid profile')
+    it('Should not respond with 200 on invalid profile')
+    it('Should have removed the profile')
+    it('Should have removed the file from the filesystem')
+  })
   /**
    * Remove the newly created account
    */
-  after('Remove the test account', () => {
-    ProfileController.deleteImageFromFileSystem(newUserImage)
-    ProfileModel.deleteOneById(newUserId)
-  })
+  // after('Remove the test account', () => {
+  //   ProfileController.deleteImageFromFileSystem(newUserImage)
+  //   ProfileModel.deleteOneById(newUserId)
+  // })
 })
