0e53ba6a7b432a0d3096e5e0cbceb2e56db8e2e1
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-12-03 00:23:27 +0000

    cleaned up

--- src/public/javascripts/components/Form.tsx
+++ src/public/javascripts/components/Form.tsx
@@ -36,11 +36,18 @@ const props = {
 interface IProps {
     exampleProp1?: string,
 }
+
+// todo :: correctly document whole file
+// todo :: find out a way to hide the notify message on focus or something
+
 class RegisterForm extends React.Component<IProps> {
 
     state: {
-        value: string,
-        submit: boolean
+        submit: boolean,
+        success: boolean,
+        name: string,
+        notifyMessage?: string,
+        notifyWith: string
     }
 
     exampleProp1: string
@@ -53,43 +60,88 @@ class RegisterForm extends React.Component<IProps> {
      */
     constructor(props: object) {
         super(props);
-        this.state = {value: '', submit: false}
+        this.state = {submit: false, success: false, name: '', notifyMessage: '', notifyWith: ''}
         this.exampleProp1 = this.props.exampleProp1 || ''
-        this.handleChange = this.handleChange.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     /**
-     * Save the value in the event object
+     * Handles the change of the name input
      *
      * @param event
      */
-    handleChange(event: any) {
-        // re renders the element on state change
-        this.setState({value: event.target.value});
+    handleNameChange(event: any) {
+        // Check the name
+        console.log('The name was changed. Setting it now')
+        this.setState({name: event.target.value})
     }
 
     /**
-     * Display last edited field value
-     *
-     * @param event
+     * Handles click of the submit button
+     * 
+     * @param event 
      */
-    handleClick(event: any) {
-        alert('The submit was clicked, and the last added value was: ' + this.state.value);
-        event.preventDefault();
-    }
-
     handleSubmit(event: any) {
+        event.preventDefault()
         console.log('Clicked submit!')
-        console.log(this.state)
-        this.setState({submit: true})
+        console.log('Sending you to the register function!')
+        this.registerProfile()
     }
 
     componentDidUpdate(prevProps: Readonly<IProps>, prevState: Readonly<{}>, snapshot?: any): void {
         console.log('Component updated!')
-        if (this.state.submit) {
-            console.log('the submit prop is true!')
+        console.log(this.state)
+        // if (this.state.submit) {
+        //     console.log('the submit prop is true!')
+        //     console.log('Sending you to the register function!')
+        //     this.registerProfile()
+        // } else {
+        // if (this.state.success  false && prevState.success !== false) {
+        //     console.log('The success of the form has change to false')
+        //     this.state.success = false
+        // }
+    }
+
+    registerProfile () {
+        console.log('Going to register the profile')
+        // Validate name
+        if (this.state.name.length < 2) {
+            console.log('The name failed validation, setting success to false')
+            this.notify(false, 'Name must be greater than 2 characters')
+            this.setState({success: false}) 
+            return
         }
+        console.log('The name meets validation! Wohoo!')
+        const form: any = $('form')[0]
+        console.log(form)
+        $.ajax({
+            method: 'post',
+            processData: false,
+            contentType: false,
+            url: '/profile/add',
+            data: new FormData(form)
+        })
+        .done((data ) => {
+            this.notify(data.success, data.message)
+        })
+        .fail((err, res, msg) => {
+            // For the real response from our controllers
+            try {
+                const data = err.responseJSON
+                this.notify(data.success, data.message)
+            } 
+            // But when a different error occurs and it doesnt return json e.g. entity file too large
+            catch {
+                console.log('failed')
+                this.notify(false, 'A problem occured whilst processing this request')
+            }
+        })
+    }
+
+    notify(success: boolean, message: string) {
+        const status = success ? 'success' : 'error'
+        this.setState({notifyMessage: message, notifyWith: status})
     }
 
     /**
@@ -104,25 +156,22 @@ class RegisterForm extends React.Component<IProps> {
      */
     render() {
         return (
-            <form id="form">
+            <form>
                 <legend>Register a Profile</legend>
                 <fieldset>
-                    <legend>Information</legend>
-                    <div className="notify"></div>
-                    <label>
-                        Name<label className="required-field">*</label>
+                    <div className={`notify ${this.state.notifyWith}`}>{this.state.notifyMessage}</div>
+                    <label className="field-container">
+                        Name<label className="required-field"> *</label>
                         <input id="name" className="form-control" name="name" placeholder="Jane Doe" type="text"
-                               onChange={this.handleChange} required/>
+                               onChange={this.handleNameChange} required/>
                     </label>
-                    <label>
+                    <label className="field-container">
                         Description
-                        <input className="form-control" name="description" placeholder="My name is Jane Doe, hello!" type="text"
-                               onChange={this.handleChange} required/>
+                        <input className="form-control" name="description" placeholder="My name is Jane Doe, hello!" type="text"/>
                     </label>
-                    <label>
+                    <label htmlFor="file-upload" className="custom-file-upload field-container">
                         Image
-                        <input className="form-control" name="image" type="file"
-                               onChange={this.handleChange} required/>
+                        <input id="file-upload" name="image" type="file"/>
                     </label>
                     <input type="submit" className="btn btn-primary" onClick={this.handleSubmit} value="Submit"/>
                 </fieldset>
