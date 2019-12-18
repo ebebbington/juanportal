988fd95fef1cc29db2c691586b625c21f567a452
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-12-18 01:39:46 +0000

    API: Cleanup - Cleaned up BaseModel test

--- src/api/tests/models/BaseModel.js
+++ src/api/tests/models/BaseModel.js
@@ -2,7 +2,6 @@ const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
 const expect = chai.expect
 
-const _ = require('lodash')
 const BaseModel = require('../../models/BaseModel')
 
 const mongoose = require('mongoose')
@@ -48,8 +47,11 @@ class TestModel extends BaseModel {
 // console.log(Test)
 
 describe('BaseModel', () => {
+
     describe('Properties', () => {
+
         describe('fieldsToExpose', () => {
+
             // Check children can access it
             class Test extends BaseModel {
                 constructor() {
@@ -57,44 +59,58 @@ describe('BaseModel', () => {
                 }
             }
             const T = new Test
+
             it('Should be protected', () => {
                 expect(T).to.haveOwnProperty('fieldsToExpose')
             })
+
             it('Should be an empty array', () => {
                 expect(T.fieldsToExpose.length).to.equal(0)
                 expect(Array.isArray(T.fieldsToExpose)).to.equal(true)
             })
+
         })
+
     })
+
     describe('Methods', () => {
+
         describe('generateObjectId', () => {
+
             const Base = new BaseModel
             const Schema = mongoose.Schema
             const ObjectIdType = Schema.ObjectId
             const stringId = '4edd40c86762e0fb12000003'
+
             it('Should return a mongoose object id on a successful parse', () => {
                 const objectId = Base.generateObjectId(stringId)
                 const isValidObjectId = mongoose.Types.ObjectId.isValid(objectId)
                 expect(isValidObjectId).to.equal(true)
             })
+
             it('Should return the passed in id when already a mongoose id', () => {
                 const objectId = mongoose.Types.ObjectId()
                 const objectId2 = Base.generateObjectId(objectId)
                 expect(objectId).to.equal(objectId2)
             })
+
             it('Should return false when failed to convert', () => {
                 const invalidStringId = '5'
                 const objectId = Base.generateObjectId(invalidStringId)
                 expect(objectId).to.equal(false)
             })
+
             it('Should be a protected method', () => {
                 const Test = new TestModel({forename: null, surname: null, age: null, postcode: null})
                 const objectId = Test.generateObjectId(stringId)
                 const isValidObjectId = mongoose.Types.ObjectId.isValid(objectId)
                 expect(isValidObjectId).to.equal(true)
             })
+
         })
+
         describe('stripNonExposableFields', () => {
+
             it('Should remove properties from a model that arent in `fieldsToExpose`', () => {
                 const rewire = require('rewire')
                 const RewiredBaseModel = rewire('../../models/BaseModel')
@@ -118,8 +134,11 @@ describe('BaseModel', () => {
                 expect(stripped.age).to.equal(exampleDoc.age)
                 expect(stripped.postcode).to.not.exist
             })
+
         })
+
         describe('fill', () => {
+
             it('Should set the matching properties in fieldsToExpose of the passed in document', () => {
                 const Test = new TestModel({forename: null, surname: null, age: null, postcode: null})
                 // Mimick a DB document
@@ -137,8 +156,11 @@ describe('BaseModel', () => {
                 } 
                 Test.fill(doc)  
             })
+
         })
+
         describe('empty', () => {
+
             it('Should empty the models properties by their fieldsToExpose', () => {
                 const Test = new TestModel({forename: null, surname: null, age: null, postcode: null})
                 const doc = {
@@ -162,6 +184,9 @@ describe('BaseModel', () => {
                 expect(Test.forename).to.equal(null)
                 expect(Test.age).to.equal(null)
             })
+
         })
+
     })
+    
 })
