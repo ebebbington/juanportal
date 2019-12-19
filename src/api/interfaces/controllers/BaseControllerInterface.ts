a49447246e99cbe71c5e58d6bdc1ea633e91ac08
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-12-19 02:11:54 +0000

    API: Fix - corrected controller interface to allow static methods

--- src/api/interfaces/controllers/BaseControllerInterface.ts
+++ src/api/interfaces/controllers/BaseControllerInterface.ts
@@ -1,7 +1,36 @@
 import express from 'express'
-interface BaseControllerInterface {
-    Post(req: express.Request, res: express.Response): void,
-    Get(req: express.Request, res: express.Response): void,
-    Update(req: express.Request, res: express.Response): void,
-    Delete(req: express.Request, res: express.Response): void
+
+// class Types {
+//     public static Get = function () {}
+
+//     }
+// }
+// interface ITest {
+//     Get: Types.Get
+// }
+
+// class Test implements ITest {
+//     public static Get () {
+
+//     }
+// }
+
+export interface IBaseController {
+    Post(req: express.Request, res: express.Response): Promise<any>,
+    Get(req: express.Request, res: express.Response): Promise<any>,
+    Update(req: express.Request, res: express.Response): Promise<any>,
+    Delete(req: express.Request, res: express.Response): Promise<any>
 }
+
+// const Test: IStaticTest = class {
+//     public static Get () {
+//         return false
+//     }
+// }
+
+// export interface BaseControllerInterface {
+//     Post(req: express.Request, res: express.Response): void,
+//     Get(req: express.Request, res: express.Response): void,
+//     Update(req: express.Request, res: express.Response): void,
+//     Delete(req: express.Request, res: express.Response): void
+// }
