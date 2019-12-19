8ebee45044c7faf0b605249df13040ca928a17e0
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-12-19 21:44:37 +0000

    API: Docs - Added some documentation for using the base controller interface

--- src/api/interfaces/controllers/BaseControllerInterface.ts
+++ src/api/interfaces/controllers/BaseControllerInterface.ts
@@ -22,6 +22,29 @@ export interface IBaseController {
     Delete(req: express.Request, res: express.Response): Promise<any>
 }
 
+/**
+ * Implement like such:
+ * 
+ * import {IBaseController} from '../interfaces/controllers/BaseControllerInterface'
+ * const ProfileController: IBaseController = class { // cant implement the interfCE UNTIL ts ALLOWS STATIC METODS IN AN INTERFACE, also this will error when requiring it, but it wont if we remove the import statement from the interface file, but even then TS throws errors when using const express = ...
+
+    public static async Get(req: express.Request<import("express-serve-static-core").ParamsDictionary>, res: express.Response): Promise<any> {
+        return 'kj'
+    }
+
+    public static async Post(req: express.Request<import("express-serve-static-core").ParamsDictionary>, res: express.Response): Promise<any> {
+        return res.render('vieq')
+    }
+
+    public static async Delete(req: express.Request<import("express-serve-static-core").ParamsDictionary>, res: express.Response): Promise<any>{
+        return false
+    }
+
+    public static async Update(req: express.Request<import("express-serve-static-core").ParamsDictionary>, res: express.Response): Promise<any> {
+        return false
+    }
+ */
+
 // const Test: IStaticTest = class {
 //     public static Get () {
 //         return false
