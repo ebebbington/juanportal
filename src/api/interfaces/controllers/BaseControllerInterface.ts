9f30e4cfb44565c6cb9f1eaa2b856fb92d3fb496
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-12-19 01:10:10 +0000

    API: Fix - Corrected the names of mthods a controller should implement

--- src/api/interfaces/controllers/BaseControllerInterface.ts
+++ src/api/interfaces/controllers/BaseControllerInterface.ts
@@ -1,7 +1,7 @@
 import express from 'express'
-export interface BaseControllerInterface {
-    post(req: express.Request, res: express.Response): void,
-    get(req: express.Request, res: express.Response): void,
-    update(req: express.Request, res: express.Response): void,
-    delete(req: express.Request, res: express.Response): void
+interface BaseControllerInterface {
+    Post(req: express.Request, res: express.Response): void,
+    Get(req: express.Request, res: express.Response): void,
+    Update(req: express.Request, res: express.Response): void,
+    Delete(req: express.Request, res: express.Response): void
 }
