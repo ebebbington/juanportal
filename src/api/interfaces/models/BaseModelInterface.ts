f91e31b2a6ffd8247aa0a9d0f2c617dba16bafb3
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-12-20 23:16:26 +0000

    API: Fix - Correctly exporting BaseModelInterface and added some docs

--- src/api/interfaces/models/BaseModelInterface.ts
+++ src/api/interfaces/models/BaseModelInterface.ts
@@ -2,8 +2,12 @@
  * Interface for each model
  * 
  * Must implement the following 
+ * 
+ * @example
+ * import {IBaseModel} from 'BaseModelInterface'
+ * class MyModel implements IBaseModel {}
  */
-interface BaseModelInterface {
+export interface IBaseModel {
     create(data: object): Promise<object>,
     readonly fieldsToExpose: string[],
     readonly tablename: string
