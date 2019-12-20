c86d7a2dcc24aaf8e06915f024bb319353a5febd
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-12-20 23:12:55 +0000

    API: Cleanup - DataInterface

--- src/api/interfaces/controllers/DataInterface.ts
+++ src/api/interfaces/controllers/DataInterface.ts
@@ -2,12 +2,12 @@
  * Anything passed into res.json() should impelemt this
  * 
  * @example
- *  import {IData} from '/path/to/this/file'
- *  const data: IData = {
- *      success: true|false,
- *      message: 'Brief message',
- *      data: any data needed to pass back
- *  }
+ * import {IData} from '/path/to/this/file'
+ * const data: IData = {
+ *   success: true|false,
+ *   message: 'Brief message',
+ *   data: any data needed to pass back
+ * }
  */
 export interface IData {
   success: boolean,
