b7a65850dd9345993b6739c56453e76c3d505722
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-12-20 23:13:38 +0000

    API: Docs - Added docs to MulterRequestInterface

--- src/api/interfaces/controllers/MulterRequest.ts
+++ src/api/interfaces/controllers/MulterRequest.ts
@@ -1,3 +1,12 @@
+/**
+ * Used to provide the "file" object to the request object in required scenarios
+ * 
+ * @example
+ * import {IMulterRequest} from 'MulterRequest'
+ * public static Post (req: IMulterRequest&express.Request) {
+ *   const file = req.file // TS shows no error
+ * }
+ */
 export interface IMulterRequest extends Request {
     file: any
 }
