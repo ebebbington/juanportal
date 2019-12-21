6975bf78e6c724bcf545951d3ff6b8e182a57b98
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-12-21 01:23:09 +0000

    API: Feature/Cleanup/Docs - ProfileModel

--- src/api/models/ProfileModel.ts
+++ src/api/models/ProfileModel.ts
@@ -10,64 +10,8 @@ const mongoose = require('mongoose')
 const logger = require('../helpers/logger')
 const BaseModel = require('./BaseModel')
 import {IBaseModel} from '../interfaces/models/BaseModelInterface'
+import { promises } from "dns";
 const Document = require('../schemas/ProfileSchema')
-
-/**
- * Profile Schema
- * 
- * Defines the baseline for the Profile model
- */
-const OLDSchema = new mongoose.Schema({
-  'name': {
-    type: String,
-    required: [true, 'Name has not been supplied'],
-    minlength: [2, 'Name is too short and should be at least 2 characters in length'],
-    maxlength: [140, 'Name is too long and should not exceed 140 characters'],
-    validate: {
-      validator: function (v: string) {
-        return /.+[^\s]/.test(v)
-      },
-      message: (props: { value: any; }) => `${props.value} is not set`
-    }
-  },
-  'description': {
-    type: String,
-    required: false,
-    maxlength: [400, 'Description is too long and should not exceed 400 characters']
-  },
-  'image': {
-    type: String,
-    required: true,
-    lowerCase: true,
-    validate: {
-      validator: function (v: string) {
-        return /\.(jpg|jpeg|JPG|JPEG|png|PNG)$/.test(v)
-      },
-      message: (props: { value: any; }) => 'Image does not have a valid extension. Please use: .jpg, .jpeg or .png'
-    },
-    minlength: [5, 'Image name is to small, therefore not a valid name'] //eg z.png
-  }
-}, {timestamps: true})
-
-// Schema.pre('save', function (next: any)  {
-//   const self: any = this
-//   Document.findOne({name: self.name}, function (err: any, profile: any) {
-//     console.log(err)
-//     console.log(profile)
-//     if (!profile) {
-//       next() 
-//     } else {
-//       logger.error('user already exists')
-//       console.log('inside save vlidation schema')
-//     }
-//   })
-// })
-/**
- * Profile Document
- * 
- * Creates a model instance of the table schema
- */
-//const OLDDocument = mongoose.model('Profile', OLDSchema)
 
 /**
  * @class ProfileModel
@@ -75,56 +19,78 @@ const OLDSchema = new mongoose.Schema({
  * @author Edward Bebbington
  * 
  * @extends BaseModel
+ * 
+ * @implements IBaseModel
  *
- * @property {number} public _id ID of profile from the database
- * @property {string} public name Name of the profile from the database
- * @property {string} pulbic description Description of the profile from the database
- * @property {string} public image Image path of the profile from the database
- * @property {string} private tablename The name of the related database table
- * @property {string[]} private fieldsToExpose The list of allowed fields to be assigned to this object
+ * @property {number}     _id             ID of profile from the database
+ * @property {string}     name            name Name of the profile from the database
+ * @property {string}     description     description Description of the profile from the database
+ * @property {string}     image           image Image path of the profile from the database
+ * @property {string}     tablename       tablename The name of the related database table
+ * @property {string[]}   fieldsToExpose  fieldsToExpose The list of allowed fields to be assigned to this object
+ * @property {string}     created_at      When the document was created
+ * @property {string}     updated_at      When the document was last updated
  * 
- * @method create Create a profile object
- * @method insertOne Insert a profile
- * @method findOneById Find a profile by their id
- * @method deleteOneById Delete a profile by their id
- * @method findTen Find the top 10 profiles
- * @method getOneByName Get a profile by their name  
- * @method deleteManyById Delete all profiles by id
  * 
- * @method fill Fill this object with database data
- * @method empty Empty this object       
- * @method validateOutputFields Strip unneeded properties from a retrieved document
- * @method validateInputFields Validate data before saving it to the db                                  
+ * @method create           Create a profile object
+ * @method findOneById      Find a profile by id
+ * @method deleteOneById    Delete a profile by their id
+ * @method deleteAll
+ * @method deleteAllByName  Delete all profiles by name
+ * @method deleteOneByName  Delete one by name
+ * @method findManyByCount  Find a defined number of profiles
+ * @method existsByName     Check if profiles exists
+ * @method findOneByName    Find one by name
+ * @method findManyByName   Find many by name
+ * 
+ * @method fill                   Fill this object with database data
+ * @method empty                  Empty this object       
+ * @method validateOutputFields   Strip unneeded properties from a retrieved document
+ * @method validateInputFields    Validate data before saving it to the db                                  
  */
 class ProfileModel extends BaseModel implements IBaseModel {
 
   /**
    * Id of the profile model
    * 
-   * @var {string} _id
+   * @var {string|null} _id
    */
   public _id: string|null = ''
 
   /**
    * Name field of the profile model
    * 
-   * @var {string} name
+   * @var {string|null} name
    */
   public name: string|null = ''
 
   /**
    * Description field of the profile model
    * 
-   * @var {string} description
+   * @var {string|null} description
    */
   public description: string|null = ''
 
   /**
    * Image field of the profile model
    * 
-   * @var {string} image
+   * @var {string|null} image
    */
   public image: string|null = ''
+
+  /**
+   * When the entry was created
+   * 
+   * @var {string|null} created_at
+   */
+  public readonly created_at: string|null = ''
+  
+  /**
+   * When the entry was updated
+   * 
+   * @var {string|null} updated_at
+   */
+  public readonly updated_at: string|null = ''
 
   /**
    * The name of the table associated with this model
@@ -155,13 +121,15 @@ class ProfileModel extends BaseModel implements IBaseModel {
    * 
    * @method create
    * 
-   * @example const Profile = new ProfileModel; const newProfile = Profile.create(...)
+   * @example
+   * const Profile = new ProfileModel;
+   * const newProfile = Profile.create(...)
    * 
-   * @param {object} data Name, description?, and image location :/public/images/randomname.extension:
+   * @param {name: string, description?: string, image: string} data
    * 
-   * @return {object} @var Document The profile model
+   * @return {void|object} Return value is set if validation errors are returned
    */
-  public async create (data: {name: string, description?: string, image: string}) {
+  public async create (data: {name: string, description?: string, image: string}): Promise<any> {
     const newProfile = new Document({
       name: data.name,
       description: data.description,
@@ -173,35 +141,12 @@ class ProfileModel extends BaseModel implements IBaseModel {
       this.fill(newProfile)
       logger.info(`[ProfileModel - create: filled the model]`)
     } catch (validationError) {
-      const fieldName = Object.keys(validationError.errors)[0]
-      const errorMessage = validationError.errors[fieldName].message
+      const fieldName: string = Object.keys(validationError.errors)[0]
+      const errorMessage: string = validationError.errors[fieldName].message
       logger.error(`Validation error: ${errorMessage}`)
       return validationError
     }
   }
-
-  /**
-   * Insert a single record
-   * 
-   * @method insertOne
-   * 
-   * @example const saved: boolean = Profile.insertOne(newProfile);
-   * 
-   * @param {object} newProfile  The profile document instance holding name, description? and image path
-   * 
-   * @return {boolean} Result of it saving or not
-   */
-  // public insertOne(newProfile: any): boolean {
-  //   try {
-  //     newProfile.save()
-  //     this.empty(this.fieldsToExpose)
-  //     this.fill(newProfile)
-  //     return true
-  //   } catch (err) {
-  //     logger.error(`error saving a profile: ${err.message}`)
-  //     return false
-  //   }
-  // }
 
   /**
    * Find a profile by the id field
@@ -209,11 +154,13 @@ class ProfileModel extends BaseModel implements IBaseModel {
    * The id passed is converted to an object id if it isnt one,
    * so you dont need to worry about converting or resetting
    * 
+   * @method findOneById
+   * 
    * @param {number} id The id of the profile to get
    * 
-   * @return {Promise} Resolved if found a profile with the profile, rejected for anything else
+   * @return {boolean} True if found a profile, else false for an empty response or unable to convert id
    */
-  public async findOneById(id: number): Promise<any>  {
+  public async findOneById(id: number): Promise<boolean>  {
     id = this.generateObjectId(id)
     if (!id) {
       return false
@@ -224,7 +171,7 @@ class ProfileModel extends BaseModel implements IBaseModel {
       // empty
       return false
     } else {
-      this.empty(this.fieldsToExpose)
+      this.empty()
       this.fill(profile)
       return true
     }
@@ -235,21 +182,25 @@ class ProfileModel extends BaseModel implements IBaseModel {
    * 
    * @method deleteOneById
    * 
-   * @example Profile.deleteOneById(id).then((success) => { console.log(`User deleted: ${success}`)})
+   * @example
+   * const success = Profile.deleteOneById(id)
    * 
    * @param {number} id Id of the profile to delete
    * 
-   * @return {Promise} False or true, depending on the success
+   * @return {boolean} False or true, depending on the success
    */
-  public async deleteOneById (id: number) {
+  public async deleteOneById (id: number): Promise<boolean> {
     id = this.generateObjectId(id)
     if (!id) {
       return false
     }
-    // delete profile
-    await Document.deleteOne({ _id: id })
-    this.empty(this.fieldsToExpose)
-    return true
+    const res = await Document.deleteOne({ _id: id })
+    if (res.ok === 1) {
+      this.empty()
+      return true
+    } else {
+      return false
+    }
   }
 
   /**
@@ -257,114 +208,125 @@ class ProfileModel extends BaseModel implements IBaseModel {
    *
    * @return {your demise and an eternity in hell}
    */
-  public static async deleteAll (key: string) {
+  public static async deleteAll (key: string): Promise<boolean> {
     const lock: string = 'Somesuperlongparameterbecauseyouneedtopassoneintothefunctionforittoworkandifyoudontitfailswhichsecuresthefunctionsoifsomeonedidwanttocallthisfunctionthentheyaregoingtohavetocopythisparametertomakeitliterallyimpossibletowriteintoanactuallyapplication'
     if (lock !== key) {
       return false
     }
     await Document.deleteMany({})
+    return true
   }
 
-  public static async deleteAllByName (name: string) {
-    await Document.deleteMany({name: name})
+  /**
+   * Delete all profiles by name. Only used for testing purposes
+   * 
+   * @method deleteAllByName
+   * 
+   * @example
+   * const success = await ProfileModel.deleteAllByName('ed')
+   * 
+   * @param {string} name Name of the profiles to delete
+   * 
+   * @return {boolean} if it succeeded and no profiles are left
+   */
+  public static async deleteAllByName (name: string): Promise<boolean> {
+    const res = await Document.deleteMany({name: name})
+    if (res.ok === 1) {
+      return true
+    } else {
+      return false
+    }
   }
 
   /**
    * Delete many profiles by their name
    * 
+   * @method deleteOneByName
+   * 
+   * @example
+   * const Profile = new ProfileModel
+   * const success = Profile.deleteOneByName('ed')
+   * 
    * @param {string} name Name of the profile to delete 
+   * 
+   * @return {boolean} success result
    */
-  public async deleteOneByName (name: string) {
-    await Document.deleteOne({name: name})
-    this.empty(this.fieldsToExpose)
+  public async deleteOneByName (name: string): Promise<boolean> {
+    const res = await Document.deleteOne({name: name})
+    if (res.ok === 1) {
+      this.empty()
+      return true
+    } else {
+      return false
+    }
   }
-
-  // public static async createMany (docs: [{name: string, description?: string, image: string}]) {
-  //   if (!docs || !docs.length) {
-  //     return true
-  //   }
-  //   let newProfiles = Document.create(docs)
-  //   // create the docs
-  //   // docs.forEach((profile: {name: string, description?: string, image: string}) => {
-  //   //   const newProfile = new Document({
-  //   //     name: profile.name,
-  //   //     description: profile.description,
-  //   //     image: profile.image
-  //   //   })
-  //   //   newProfiles.push(newProfile)
-  //   // })
-
-  //   // validate each profile
-  //   // const errors = newProfiles.forEach((profile: any) => {
-  //   //   const err = profile.validateSync()
-  //   //   if (err) {
-  //   //     return err
-  //   //   }
-  //   // })
-  //   // if (errors) {
-  //   //   return errors
-  //   // }
-
-  //   // insert
-  //   // if (!errors) {
-  //     // console.log('no errors when validation')
-  //     console.log('going to insert many with a len of: ' + docs.length)
-  //     const errors = await Document.insertMany(docs)
-  //     console.log(errors)
-
-  //     console.log('seemed to save')
-  //     return false
-  //   // }
-  // }
 
   /**
    * Find many profiles by a specified amount
    * 
+   * @method findManyByCount
+   * 
+   * @example
+   * const profiles: [objects] = ProfileModel.findManyByCount(102) 
+   * 
    * @param {number} amount  The amount of profiles to find
    * 
-   * @return {[{}]} Profiles or false if not found any
+   * @return {[profiles]|boolean} array of profiles or empty array
    */
-  public static async findManyByCount (amount: number) {
+  public static async findManyByCount (amount: number): Promise<boolean|Array<any>> {
     if (!amount) {
-      logger.debug('[ProfileModel findManyByCount - Param isnt defined')
-      return []
+      logger.error('[ProfileModel findManyByCount - Param isnt defined')
+      return false
     }
-      logger.debug('Going to find many profiles')
-      const profiles: any = await Document.find({}).sort({'date': -1}).limit(amount)
-      return profiles
+    const cap = 100
+    if (amount > cap) amount = cap
+    const profiles: any = await Document.find({}).sort({'date': -1}).limit(amount)
+    // convert to array if only one is found
+    if (Array.isArray(profiles) && !profiles.length || !profiles) {
+      return false
+    }
+    if (!profiles.length)
+      return [profiles]
+    return profiles
   }
 
   /**
    * Check if a profile exists by their name
    * 
+   * @method existsByName
+   * 
+   * @example
+   * const exists = ProfileModel.existsByName('ed'); // true | false
+   * 
    * @param {string} name Name of the profile to find
    * 
-   * @return {boolean}
+   * @return {boolean} if they exist
    */
-  public static async existsByName (name: string) {
+  public static async existsByName (name: string): Promise<boolean> {
     const profile = await Document.findOne({name: name})
-    console.info('showing res from exists function')
-    console.info(profile)
     return profile ? true : false
   }
 
   /**
    * Get a profile by a name
    * 
+   * @method findOneByName
+   * 
+   * @example
+   * const Profile = new ProfileModel
+   * const success = await Profile.findOneByName('ed')
+   * 
    * @param {string} name Name of the profile to find 
    * 
-   * @return {promise} Resolved if found, rejected if not
+   * @return {boolean} is profile was found
    */
-  public async findOneByName (nameOfProfile: string) {
+  public async findOneByName (nameOfProfile: string): Promise<boolean> {
       const profile = await Document.findOne({name: nameOfProfile})
       if (Array.isArray(profile) && !profile.length || !profile) {
-        logger.debug('Empty result from findonevbyname')
         // empty
         return false
       } else {
-        logger.debug('Found a profile in findonebyname: ')
-        logger.debug(profile)
-        this.empty(this.fieldsToExpose)
+        this.empty()
         this.fill(profile)
         return true
       }
@@ -373,18 +335,26 @@ class ProfileModel extends BaseModel implements IBaseModel {
   /**
    * Find many profiles by their name
    * 
+   * @method findManyByName
+   * 
+   * @example
+   * const profiles = await ProfileModel.findManyByName('ed')
+   * 
    * @param name Name of the profiles to find
    * 
-   * @return {[{}]} Profiles found
+   * @return {boolean|[profiles]} Profiles found
    */
-  public static async findManyByName (name: string) {
+  public static async findManyByName (name: string): Promise<boolean|Array<any>> {
     const profiles = await Document.find({name: name})
     if (Array.isArray(profiles) && !profiles.length || !profiles) {
-      logger.debug('Empty result from findonevbyname')
       // empty
       return false
     }
-    return profiles
+    if (Array.isArray(profiles)) {
+      return profiles
+    } else {
+      return [profiles]
+    }
   }
 }
 
