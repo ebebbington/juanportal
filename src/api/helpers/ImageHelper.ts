ed399db3647c2317e8d9c534ecf64fead51a043b
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-12-22 23:02:22 +0000

    API: Feature - Simplified ImageHelper and documented it

--- src/api/helpers/ImageHelper.ts
+++ src/api/helpers/ImageHelper.ts
@@ -1,150 +1,48 @@
-import fs from 'fs'
-const _ = require('lodash')
-const logger = require('./logger')
-const { imagesDir, rootDir } = require('../api.config.js')
+const path = require('path')
 
 /**
  * @class ImageHelper
  * 
  * @author Edward Bebbington
  * 
- * @method createNewFilename Generate a filename
- * @method getExtension Get the extension of a file
- * @method saveToFS Save a file to the file system
- * @method existsOnFS Check if a file exists on the file system
+ * @description Class to aid in image file related operations
+ * 
+ * @example
+ * const ImageHelper = require('ImageHelper')
+ * ...
+ * ... = ImageHelper.[...]
+ * 
  * @method generateRandomName Generate a 36 character string
- * @method deleteFromFS Delete a file from the file system
  */
 class ImageHelper {
 
     /**
-     * Create a random long string including the extension of the image filename
+     * Generate a random 36 character string, keeping the extension intact
      * 
-     * @param {string} filename The file name with the extension
+     * @method generateRandomName
      * 
-     * @return {string|bool} the new file name with the extension from the original name
+     * @example
+     * const ImageHelper = require('ImageHelper')
+     * let exampleFileName = 'paris-disneyland-2019.PNG'
+     * exampleFileName = ImageHelper.generateRandomName(exampleFileName) // `[a-zA-Z]{36}`.PNG
+     * 
+     * @param {string} filename Name of the file, only to get the extension of
+     * 
+     * @return {string|boolean} The random string | no extension
      */
-    public createNewFilename(filename: string): string|boolean {
-        const supportedExtensions = [
-            'jpg',
-            'jpeg',
-            'JPG',
-            'JPEG',
-            'png',
-            'PNG'
-        ]
-        const splitFileName = filename.split('.')
-        const endSplit = splitFileName[splitFileName.length - 1]
-        const hasExtension = supportedExtensions.includes(endSplit)
-        if (!hasExtension) {
+    public static generateRandomName(filename: string): string|boolean {
+        const extension = path.extname(filename)
+        if (!extension) {
             return false
         }
-        const randomString = this.generateRandomName()
-        const extension = this.getExtension(filename)
-        const newFilename = randomString + extension
-        return newFilename
-    }
-
-    /**
-     * Get the extension of a file name
-     * 
-     * @param {string} filename The filename to check
-     * 
-     * @return {string} The extension or if not present, default to .jpg
-     */
-    private getExtension(filename: string): string|boolean {
-        if (!filename) {
-            return '.jpg'
-        }
-        const extArr = filename.split('.')
-        // when no extension was passed in, or just an invalid name
-        if (extArr.length === 1) {
-            return false
-        }
-        const ext = _.last(extArr)
-        return ext ? '.' + ext : '.jpg'
-    }
-
-    /**
-     * Save an image to the servers filesystem and check it exists
-     * 
-     * @param {string} filename The randomised filename and extension
-     * @param {object} file The file object passed in with the request
-     * 
-     * @return {boolean} False if it still exists or an error occured, true if successed
-     */
-    public saveToFS(filename: string, file: any): boolean {
-        if (!filename) {
-            logger.debug('No filename was passed in to save to fs')
-            return false
-        }
-        if (file) {
-            logger.debug('file has been passed in')
-            try {
-                fs.createWriteStream(imagesDir + filename).write(file.buffer)
-                return this.existsOnFS(filename)
-            } catch (e) {
-                logger.error(e)
-                return false
-            }
-        }
-        // Else just copy the default image
-        if (!file) {
-            logger.debug('no file was passed in')
-            fs.createReadStream(imagesDir + 'sample.jpg').pipe(fs.createWriteStream(imagesDir + filename))
-            return this.existsOnFS(filename)
-        }
-        return false
-    }
-
-    /**
-     * Check if a image exists by the filename
-     * 
-     * @param {string} name The name of the file
-     * 
-     * @return {boolean} if it exists 
-     */
-    private existsOnFS(name: string): boolean {
-        logger.debug(['file name to check if exists',name])
-        const fullPath: string = imagesDir + name
-        logger.debug(fullPath)
-        logger.debug(fs.existsSync(fullPath))
-        return fs.existsSync(fullPath) ? true : false
-    }
-
-    /**
-     * Generate a random 36 character string
-     * 
-     * @return {string} The random string
-     */
-    private generateRandomName(): string {
-        return Math.random().toString(36).substring(2, 8)
+        const randomString = Math.random().toString(36).substring(2, 8)
             + Math.random().toString(36).substring(2, 8)
             + Math.random().toString(36).substring(2, 8)
             + Math.random().toString(36).substring(2, 8)
             + Math.random().toString(36).substring(2, 8)
             + Math.random().toString(36).substring(2, 8)
-    }
-
-    /**
-     * Delete a file from the filesystem
-     * 
-     * @param {string} imageName The image name to find
-     * 
-     * @return {boolean} If it still exists
-     */
-    public deleteFromFS(imageName: string): boolean {
-        const pathToImage = rootDir + imageName // image path is: /public/images/...
-        logger.debug(pathToImage)
-        // delete image
-        try {
-            fs.unlinkSync(pathToImage)
-        } catch (err) {
-            // but disregard as there isnt anything to delete
-            logger.error(err)
-        }
-        logger.debug('going to check if ' + imageName + ' exists')
-        return this.existsOnFS(imageName)
+        const newFileName = randomString + extension
+        return newFileName
     }
 
 }
