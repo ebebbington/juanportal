3a918799a0980fd03b6316af8659a389fb788f03
Edward Bebbington <EdwardSBebbington@hotmail.com>
2019-12-23 00:18:26 +0000

    JuanPortal: Feature - Added baseline of tests for image helper and improved upon the base file

---
+++ src/juanportal/helpers/ImageHelper.ts
@@ -0,0 +1,94 @@
+import fs from 'fs'
+const _ = require('lodash')
+const logger = require('./logger')
+const { imagesDir, rootDir } = require('../juanportal.config.js')
+
+/**
+ * @class ImageHelper
+ * 
+ * @author Edward Bebbington
+ * 
+ * @description Uaed to aid in image file related actions
+ * 
+ * @example
+ * const ImageHelper = require('ImageHelper')
+ * 
+ * 
+ * @method saveToFS Save a file to the file system
+ * @method existsOnFS Check if a file exists on the file system
+ * @method deleteFromFS Delete a file from the file system
+ */
+class ImageHelper {
+
+    /**
+     * Save an image to the servers filesystem and check it exists
+     * 
+     * @param {string} filename The randomised filename and extension
+     * @param {object} file The file object passed in with the request
+     * 
+     * @return {boolean} False if it still exists or an error occured, true if successed
+     */
+    public saveToFS(filename: string, file: any): boolean {
+        logger.info('[ImageHelper - saveToFS]')
+        if (!filename) {
+            logger.debug('No filename was passed in to save to fs')
+            return false
+        }
+        if (file) {
+            logger.info('file has been passed in')
+            try {
+                fs.createWriteStream(imagesDir + filename).write(file.buffer)
+                return this.existsOnFS(filename)
+            } catch (e) {
+                logger.error(e)
+                return false
+            }
+        }
+        // Else just copy the default image
+        if (!file) {
+            logger.info('No file was passed in')
+            fs.createReadStream(imagesDir + 'sample.jpg').pipe(fs.createWriteStream(imagesDir + filename))
+            return this.existsOnFS(filename)
+        }
+        return false
+    }
+
+    /**
+     * Check if a image exists by the filename
+     * 
+     * @param {string} name The name of the file
+     * 
+     * @return {boolean} if it exists 
+     */
+    private existsOnFS(name: string): boolean {
+        logger.debug(['file name to check if exists',name])
+        const fullPath: string = imagesDir + name
+        logger.debug(fullPath)
+        logger.debug(fs.existsSync(fullPath))
+        return fs.existsSync(fullPath) ? true : false
+    }
+
+    /**
+     * Delete a file from the filesystem
+     * 
+     * @param {string} imageName The image name to find
+     * 
+     * @return {boolean} If it still exists
+     */
+    public deleteFromFS(imageName: string): boolean {
+        const pathToImage = rootDir + imageName // image path is: /public/images/...
+        logger.debug(pathToImage)
+        // delete image
+        try {
+            fs.unlinkSync(pathToImage)
+        } catch (err) {
+            // but disregard as there isnt anything to delete
+            logger.error(err)
+        }
+        logger.debug('going to check if ' + imageName + ' exists')
+        return this.existsOnFS(imageName)
+    }
+
+}
+
+module.exports = ImageHelper
