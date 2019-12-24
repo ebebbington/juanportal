895fec16daf4c6bff96471d5c9f9f85f9ddcdefa
ebebbington <EdwardSBebbington@hotmail.com>
2019-12-24 23:28:49 +0000

    API: Feature - added the getMongooseDocument to the BaseModelInterface

--- src/api/interfaces/models/BaseModelInterface.ts
+++ src/api/interfaces/models/BaseModelInterface.ts
@@ -9,6 +9,7 @@
  */
 export interface IBaseModel {
     create(data: object): Promise<object>,
+    getMongooseDocument(): Document,
     readonly created_at: string|null,
     readonly updated_at: string|null,
     readonly fieldsToExpose: string[],
