4688c00b8e40146348704b46c6fb4dfcc54925d9
ebebbington <EdwardSBebbington@hotmail.com>
2019-12-28 19:21:25 +0000

    JP: Cleanup - removed old files for interfaces

--- src/juanportal/interfaces/controllers/BaseControllerInterface.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-export interface BaseControllerInterface {
-    post(req: express.Request, res: express.Response): void,
-    get(req: express.Request, res: express.Response): void,
-    update(req: express.Request, res: express.Response): void,
-    delete(req: express.Request, res: express.Response): void
-}
