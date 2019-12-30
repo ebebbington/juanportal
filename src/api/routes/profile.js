911c4cd6ec7a98ec20853d0b9c18d28c8d1d3415
ebebbington <EdwardSBebbington@hotmail.com>
2019-12-30 00:46:25 +0000

    API: Docs - added some examples for routes in profile

--- src/api/routes/profile.js
+++ src/api/routes/profile.js
@@ -7,13 +7,56 @@ const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
 
 app.route('/count/:count')
+
+  /**
+   * @example
+   * const numberOfProfiles = 100
+   * $.ajax({
+   *  url: '/api/profile/count/numberOfProfiles',
+   *  method: 'get',
+   *  dataType: 'json'
+   * })
+   */
   .get(ProfileController.GetProfilesByAmount)
 
 app.route('/id/:id')
+
+    /**
+     * @example
+     * const id = 'get the id here
+     * $.ajax({
+     *  url: '/api/profile/id/' + id,
+     *  method: 'get',
+     *  dataType: 'json'
+     * })
+     */
   .get(ProfileController.GetProfileById)
+
+  /**
+     * @example
+     * const id = 'get the id here
+     * $.ajax({
+     *  url: '/api/profile/id/' + id,
+     *  method: 'delete',
+     *  dataType: 'json'
+     * })
+     */
   .delete(ProfileController.DeleteProfileById)
 
 app.route('/')
+
+  /**
+   * @example
+   * const form = $('form')[0]
+   * $.ajax({
+      url: '/api/profile,
+      method: 'post',
+      processData: false,
+      contentType: false,
+      dataType: 'json',
+      data: new FormData(form)
+    })
+   */
   .post(upload.single('image'), ProfileController.PostProfile)
 
 module.exports = app
