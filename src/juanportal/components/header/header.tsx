35e3e5feca2aad09b3db63dbc732b3d81372b163
ebebbington <EdwardSBebbington@hotmail.com>
2020-01-08 22:05:29 +0000

    JP: FT - Added and and completed the header component

---
+++ src/juanportal/components/header/header.tsx
@@ -0,0 +1,61 @@
+import React, { useState, ReactElement, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+//@ts-ignore
+import headerStyles from './header.module.css'
+
+const Header = () => {
+
+    const [title, setTitle] = useState('')
+
+    const [menuExpanded, setMenuExpanded] = useState(false)
+
+    const url: string = window.location.pathname
+
+    useEffect(() => {
+        /* / */
+        if (url === '/')                        setTitle('Home')
+        /* /profile/view/:id */
+        if (url.indexOf('/profile/id/') > -1)   setTitle('View Profile')
+        /* /profile/add */
+        if (url === '/profile/add')             setTitle('Add Profile')
+    })
+
+    const handleMenuClick = () => {
+        // Opposite of what the value already is
+        setMenuExpanded(!menuExpanded)
+        // Because `menuExpanded` wont show us the changes yet, we need to define it ourselves
+        const isExpanded: boolean = !menuExpanded
+    }
+
+    const componentDidMount = () => {
+        console.log('Menu Expanded: ' + menuExpanded)
+    }
+    componentDidMount()
+
+    const hidden: string = !menuExpanded ? 'hidden' : ''
+
+    return (
+        <div className={headerStyles.header}>
+            <div className={headerStyles.navMenuTrigger}>
+                <button className="btn" onClick={handleMenuClick}>
+                    <i className={`fa fa-2x ${menuExpanded ? 'fa-times' : 'fa-bars'}`}></i>
+                </button>
+                <div className={`${!menuExpanded ? headerStyles.hide : ''} ${headerStyles.navMenu}`}>
+                    <ul className={headerStyles.navMenuList}>
+                        {url !== '/' &&<li className={headerStyles.navMenuListItem}><a href="/"><h4>Home</h4></a></li>}
+                        {url.indexOf('/profile/add/') < 0 &&<li className={headerStyles.navMenuListItem}><a href="/profile/add"><h4>Add Profile</h4></a></li>}
+                    </ul>
+                </div>
+            </div>
+            <div className={headerStyles.titleHolder}>
+                <h1>
+                    <strong>
+                        <i className={headerStyles.title}>{title ? title : 'Loading...'}</i>
+                    </strong>
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(<Header />, document.getElementById('header'))
