7172418c7c9f9bb671d21f1bc0df8408ac9a80c4
ebebbington <EdwardSBebbington@hotmail.com>
2020-01-18 15:38:31 +0000

    JP: FT - Added docs to the slider component and made it implement an interface for props

--- src/juanportal/components/slider/slider.tsx
+++ src/juanportal/components/slider/slider.tsx
@@ -5,10 +5,30 @@ import { getStylings } from './util'
 const sliderStylings = getStylings()
 
 interface IProps {
-
+    title: string,
+    setChecked: boolean
 }
 
-const Slider = ({title, setChecked}) => {
+/**
+ * @name Slider
+ * 
+ * @description
+ * The slider component is reponsible for any markup that requires a slider (yes or no style)
+ * 
+ * @example
+ * import Slider from '../slider/slider'
+ * const Test = () => {
+ *   return (
+ *     <div onClick={() => handleSliderClick}> // extra specific logic such as a fetch
+ *       <Slider text="PIN" setChecked={true|false} />
+ *     </div>
+ *   )
+ * }
+ * 
+ * @param {string} title Title to accompany the slider with, a 1-2 word description
+ * @param {boolean} setChecked Tell the component to check the input checkbox
+ */
+const Slider = ({title, setChecked}: IProps) => {
     
     const [isChecked, setIsChecked] = useState(setChecked)
     const [iconClass, setIconClass] = useState(setChecked ? 'fa-check' : 'fa-cross')
