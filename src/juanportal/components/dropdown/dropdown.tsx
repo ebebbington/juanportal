0a30bc19e95bf5343a7212b724f52d85df663298
ebebbington <EdwardSBebbington@hotmail.com>
2020-01-19 00:55:51 +0000

    JP: FT - Tested and completed and fixed dropdown component

--- src/juanportal/components/dropdown/dropdown.tsx
+++ src/juanportal/components/dropdown/dropdown.tsx
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom'
 import { getStylings } from './util'
 const dropdownStylings = getStylings()
 
+interface ILiData {
+    text: string,
+    checked: boolean,
+    id?: any
+}
 interface IProps {
     title: string,
-    listItemsData: any
+    liData: Array<ILiData>,
+    checkedHandler?: Function
 }
 
 /**
@@ -17,37 +23,67 @@ interface IProps {
  * @example
  * import Dropdown from '../dropdown/dropdown'
  * const Test = () => {
- *   const data = [{text: 'Simon', checked: true}, ...]
+ *   const dataToPopulateULWith = [{text: 'Simon', checked: true, id?: 1003}, {...}]
  *   const title = 'Friends'
+ *   const handleCheck = ({text: string, id?: string, checked: boolean}) {
+ *     console.log('A checkbox was modified! Here is the data of that list item:')
+ *     console.log('The text: ' + text + '. Is it checked: ' + checked + '. The id for the data: ' + id)
+ *   } 
  *   return (
- *     <Dropdown title={title} listItemData={data} />
+ *     <Dropdown title={title} liData={data} checkedHandler={handleCheck} />
  *   )
  * }
  * 
  * @param {string} title Title to give the dropdown
- * @param {Array<{checked: boolean, text: string}} listItemsData Data used to populate the list items
+ * @param {Array<{text: string, id?: string, checked: boolean}>} liData Data used to populate the list items, where `id` can be used to identify certain data
+ * @param {Function} checkedHandler On change of input, calls this function passing in the text, checked, and id data should the calling component wish to do extra logic when data is changed
  */
-const Dropdown = ({title, listItemsData}: IProps) => {
+const Dropdown = ({title, liData, checkedHandler}: IProps) => {
+
+    // Just loop through the data provided to get the number of checked
+    // items, to display this count on initial load
+    let countOfChecked = 0
+    liData.forEach((data: any) => {
+        if (data.checked) countOfChecked += 1
+    });
+    const [selected, setSelected] = useState(countOfChecked)
+
+    const handleChange = (event: any) => {
+        const text: string = event.target.value
+        const isChecked: boolean = event.target.checked ? true : false
+        const id: string = event.target.dataset.id
+        const passBackData: {
+            text: string,
+            id?: string,
+            checked: boolean
+        } = {
+            text: text,
+            id: id,
+            checked: isChecked
+        }
+        // Show how many are selected for this component
+        setSelected(isChecked ? selected + 1 : selected - 1)
+        // Then call passed in function to let them do anything they need with the data
+        if (checkedHandler) {
+            checkedHandler(passBackData)
+        }
+    }
 
     return (
         <div className={dropdownStylings.dropdownContainer}>
-            <p>
-                {title}
-            </p>
-            <button id="dropdown-description" className="btn" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" title={`Open up the dropdown for ${title}`}>
-                Number selected
-                <i className="caret"></i>
+            <p>{title}</p>
+            <button id="dropdown-description" className="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" title={`Open up the dropdown for ${title}`}>
+                {selected} Selected
             </button>
-            <ul className="dropdown-menu list-group" aria-labelledby="dropdown-description">
-            {listItemsData.map((listItemData: any) => {
-                <li>
+            <ul className="dropdown-menu" aria-labelledby="dropdown-description">
+            {liData.map((data: any) => 
+                <li key={data.text}>
                     <label>
-                        <input type="checkbox" {...listItemsData.checked}>
-                            {listItemsData.text}
-                        </input>
+                        <input type="checkbox" defaultChecked={data.checked} onChange={event => handleChange(event)} data-id={data.id} value={data.text}></input>
+                        {data.text}
                     </label>
                 </li>
-            })}
+            )}
             </ul>
         </div>
     )
