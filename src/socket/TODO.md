75abe6ed5820085775bcf26b0722b730dba557f1
ebebbington <EdwardSBebbington@hotmail.com>
2020-01-23 01:57:07 +0000

    Socket: FT - Managed to get the Socket app in working order

---
+++ src/socket/TODO.md
@@ -0,0 +1,3 @@
+* Sort out redis side of the flash app
+* create all required handlers for the websockets
+* remember to include typings e.g. my_var: str = 'hello'
--- src/socket/app.py
+++ src/socket/app.py
@@ -1,53 +1,59 @@
+# Initialise the following on the client to connect to the socket:
+#script(src="//cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js" integrity="sha256-yr4fRk/GU1ehYJPAs8P4JlTgu0Hdsp4ZKrx8bDEDC3I=" crossorigin="anonymous")
+    #script(type="text/javascript" charset="utf-8").
+        #var socket = io();
+
 from rediscommunicator import RedisCommunicator
-from flask import Flask
-import sys
-import optparse
+from flask import Flask, render_template
 import redis
-import time
 from flask_socketio import SocketIO, send, emit
-import json
 
 # initialise our app with flask
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 
 # create the socket io instance
-socketIo = SocketIO(app)
+socketio = SocketIO(app)
 
+# Setup redis
 RedisCommunicator = RedisCommunicator()
 RedisCommunicator.start_listening()
 
+some_var: str = 'hi'
+
 # Functions defined with @app.route('*') are "view functions", they have to "return" and cant "print"
 
-@app.route("/socket")
+@app.route("/flask") # based on nginx configs, location block for /socket would match /socket here, acts as the root url
 def hello_world():
-    app.logger.info('You hit the /socket route method')
-    return "Hello world from Distelli & Docker!"
+    print('Going to render our view :)')
+    return render_template('index.html', title = 'Socket - Home', data = 'Call me Bond, Data Bond')
 
-@app.route('/socket/flask')
-def goodbye_world():
-    app.logger.info('You hit the /socket/flash route method')
-    return 'Goodbye!'
+@socketio.on('chat message')
+def handle_ws_chat_message(message):
+    print('Handling an incoming chat message for the web socket. Received message is: {}'.format(message))
 
-@app.route('/socket.io')
-def test(a):
-    app.logger.info('hey')
+@socketio.on('message')
+def handle_ws_general_event():
+    print('Incoming web socket request without a subject')
 
-@socketIo.on('chat message')
-def handle_message(message):
-    app.logger.info('You hit the * socket message method')
-    print('received message: ' + message)
+@socketio.on('json')
+def gggg():
+    print('does this ever even get fired?')
 
-@socketIo.on('chat message')
-def hh(message):
-    app.logger.info('You hit the * socket message method')
-    print('received message: ' + message)
+@socketio.on('user joined')
+def handle_ws_user_joined(username):
+    print('A user has joined the pool')
+
+@socketio.on('connect')
+def handle_ws_connect():
+    print('Incoming successful connection for websockets!')
+    emit('chat message', 'hey')
+
+@socketio.on('disconnect')
+def handle__ws_disconnect():
+    print('A web socket connection has just disconnected')
+
 
 if __name__ == '__main__':
-    parser = optparse.OptionParser(usage="python app.py -p ")
-    parser.add_option('-p', '--port', action='store', dest='port', help='The port to listen on.')
-    (args, _) = parser.parse_args()
-    if args.port == None:
-        print "Missing required argument: -p/--port"
-        sys.exit(1)
-    app.run(host='0.0.0.0', port=int(args.port), debug=True)
-    socketIo.run(app)
+    #app.run(host='0.0.0.0', port=9009, debug=True) # port = port the container is running on
+    socketio.run(app, host='0.0.0.0', port=9009, debug=True)
