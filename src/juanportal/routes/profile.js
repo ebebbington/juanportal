5e948d1e78631e8cd16b9616503691cfa86acca9
Edward Bebbington <47337480+ebebbington@users.noreply.github.com>
2020-10-05 00:56:17 +0100

    Improvement (#22)
    * fix: dont delete default image file
    
    * chore: improve upon the crappy font used
    
    * feat: fetch more profiles when none are left on screen

--- src/juanportal/routes/profile.js
+++ src/juanportal/routes/profile.js
@@ -62,6 +62,9 @@ app.route('/image')
   .delete(upload.single('image'), (req, res) => {
     // todo add checks so people just cant willy nilly send requests e.g. JWT
     const filename = req.query.filename
+    if (filename.indexOf("sample.jpg") > -1) {
+      return res.status(200).json({success: true, message: "Didn't delete as it is our default picture. But it's all good :)"})
+    }
     const Image = new ImageHelper
     // check it exists first
     const exists = Image.existsOnFS(filename)
