3d0363b09b6537bcb697da4abbe36853eee68797
Edward Bebbington <47337480+ebebbington@users.noreply.github.com>
2021-01-04 18:20:16 +0000

    Update profile.js

--- src/api/routes/profile.js
+++ src/api/routes/profile.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const ProfileController = require('../controllers/ProfileController')
+const t = require("../a")
 
 const multer = require('multer')
 const storage = multer.memoryStorage()
