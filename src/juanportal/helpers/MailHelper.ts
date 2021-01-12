364424610ab767716c1ef63a4553bb27b9f38fae
Edward Bebbington <edward.bebbington@intercity.technology>
2021-01-12 23:48:16 +0000

    debug

--- src/juanportal/helpers/MailHelper.ts
+++ src/juanportal/helpers/MailHelper.ts
@@ -3,14 +3,15 @@
 import nodemailer from "nodemailer";
 import config from "../juanportal.config";
 import logger from "./logger";
-// import dotenv from "dotenv";
-// dotenv.config();
+import dotenv from "dotenv";
+dotenv.config();
 const mailPassword = process.env.MAIL_PASSWORD;
 const mail = {
   email: config.mail.email,
   pass: mailPassword,
   user: config.mail.user,
 };
+console.log(mail);
 
 interface NodeMailerSendResponse {
   accepted: string[]; // email addressed sent to
