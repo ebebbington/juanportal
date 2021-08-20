059fa70aa1acca7d5a8efea3e11f3daf8675234f
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-08-20 16:50:36 +0100

    fix

--- src/juanportal/jest.config.js
+++ src/juanportal/jest.config.js
@@ -22,7 +22,7 @@ module.exports = {
   moduleNameMapper: {
     "\\.(css|less)$": "identity-obj-proxy",
   },
-  testEnvironment: "jsdom"
+  testEnvironment: "jsdom",
   //testURL: "http://localhost:9002"
   //rootDir: './tests/components',
   //testMatch: ['*.spec.js']
