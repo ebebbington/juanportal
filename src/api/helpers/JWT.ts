b1d73d46d4da1fec1c8e92b7923bcf1a14386fd4
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-09-05 20:12:42 +0100

    debug

--- src/api/helpers/JWT.ts
+++ src/api/helpers/JWT.ts
@@ -59,12 +59,11 @@ class JWT {
       logger.info("Verified JWT in request");
       next();
     } catch (err) {
-      if (err instanceof Error) {
-        logger.error(`Failed to verify the token: ${err.message}`);
-        return res
-          .status(403)
-          .json({ success: false, message: err.message, data: token });
-      }
+      const { message } = (err as { message: string})
+      logger.error(`Failed to verify the token: ${message}`);
+      return res
+        .status(403)
+        .json({ success: false, message, data: token });
     }
   }
 
@@ -100,8 +99,9 @@ class JWT {
       const token = jwt.sign(payload, privateKey, options as jwt.SignOptions);
       return token;
     } catch (err) {
+      const { message } = err as { message: string }
       if (err instanceof Error) {
-        logger.error(`Failed to sign the token: ${err.message}`);
+        logger.error(`Failed to sign the token: ${message}`);
       }
       return false;
     }
